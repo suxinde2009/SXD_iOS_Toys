319b737019612bc4ab60af93d4d4a26ef49eb5f4
sxd <suxinde2009@126.com>
2014-11-24 01:39:04 +0800

    BLE Lib

---
+++ XDBleLib/XDBleLib/XDBleLib.h
@@ -0,0 +1,13 @@
+//
+//  XDBleLib.h
+//  XDBleLib
+//
+//  Created by su xinde on 14/11/24.
+//  Copyright (c) 2014年 su xinde. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XDBleLib : NSObject
+
+@end
---
+++ XDBleLib/XDBleLib/XDBleLib.m
@@ -0,0 +1,13 @@
+//
+//  XDBleLib.m
+//  XDBleLib
+//
+//  Created by su xinde on 14/11/24.
+//  Copyright (c) 2014年 su xinde. All rights reserved.
+//
+
+#import "XDBleLib.h"
+
+@implementation XDBleLib
+
+@end
