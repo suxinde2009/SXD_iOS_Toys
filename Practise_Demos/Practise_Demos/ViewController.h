a2d379bf1b566f12f63c8f81a37dec5ebe6e261f
suxinde <suxinde2009@126.com>
2015-06-07 14:17:22 +0800

    Practise Demo initialization

---
+++ Practise_Demos/Practise_Demos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Practise_Demos
+//
+//  Created by suxinde on 15/6/7.
+//  Copyright (c) 2015年 suxinde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Practise_Demos/Practise_Demos/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Practise_Demos
+//
+//  Created by suxinde on 15/6/7.
+//  Copyright (c) 2015年 suxinde. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
